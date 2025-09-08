604fd43f3731658c783f1b287af64e16d2040930
Clemens Elflein <clemens1@familie-elflein.de>
2025-09-08 23:51:01 +0200

    Added error and success message helpers

--- console.py
+++ console.py
@@ -11,3 +11,11 @@ def info(message: str) -> None:
 def debug(message: str) -> None:
     """Print a debug message in a consistent format."""
     print(f"[bold grey]Debug:[/bold grey] {message}")
+
+def error(message: str) -> None:
+    """Print an error message in a consistent format."""
+    print(f"[bold red]:cross_mark: Error:[/bold red] {message}")
+
+def success(message: str) -> None:
+    """Print a success message in a consistent format."""
+    print(f"[bold green]:heavy_check_mark:[/bold green] {message}")
