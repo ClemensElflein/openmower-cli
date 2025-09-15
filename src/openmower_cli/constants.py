10b5225eb5ae730de58bebd855d4fce530d3bb6a
Clemens Elflein <clemens1@familie-elflein.de>
2025-09-15 15:21:55 +0200

    Rename default container name from openmower to open_mower_ros

--- src/openmower_cli/constants.py
+++ src/openmower_cli/constants.py
@@ -25,7 +25,7 @@ except Exception:
 COMPOSE_FILE: str = os.environ.get("OPENMOWER_COMPOSE_FILE", "/opt/stacks/openmower/compose.yaml")
 DOCKER_BIN: str = os.environ.get("OPENMOWER_DOCKER_BIN", "/usr/bin/docker")
 STACK_NAME: str = os.environ.get("OPENMOWER_STACK_NAME", "openmower")
-DEFAULT_SERVICE: str = os.environ.get("OPENMOWER_DEFAULT_SERVICE", "openmower")
+DEFAULT_SERVICE: str = os.environ.get("OPENMOWER_DEFAULT_SERVICE", "open_mower_ros")
 
 # GitHub repo for self-update and update checks
 DEFAULT_GH_REPO: str = os.environ.get("OPENMOWER_CLI_REPO", "ClemensElflein/openmower-cli")
